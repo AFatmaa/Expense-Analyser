2ddd95b33b87fcf27561034e0c7242b8c1e12ee6
Fatma <fatma.arslantas@outlook.com>
2026-03-26 20:35:30 +0000

    Add total expense calculation

--- main.py
+++ main.py
@@ -15,10 +15,20 @@ def read_expenses(file_name):
       expenses.append(expense)
     
     return expenses
+  
+def calculate_total(expenses):
+  total = 0
+
+  for expense in expenses:
+    total += expense["amount"]
+
+  return total
 
 def main():
   expenses = read_expenses("expenses.csv")
-  print(expenses)
+  total = calculate_total(expenses)
+  
+  print(total)
 
 if __name__ == "__main__":
   main()
